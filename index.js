143c379d5506442d54ad6d957a00808c51cb02d5
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-03-11 08:15:17 -0400

    upd
    start

--- index.js
+++
@@ -1,7 +0,0 @@
-// const nodemon = require("nodemon"); <= import from package
-
-const obj = require("./products");
-
-console.log(obj);
-const { products } = require("./products");
-console.log(products);
